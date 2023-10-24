c44ce51040690ccdb5910a64e7b64dc740b18eea
Chidiebere Chukwudi <chidideveloper@gmail.com>
2023-10-24 09:26:01 +0100

    added hydrator class to our service dependecny so that laravvel can resolve it

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace Jovialcore\LaravelDto\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Jovialcore\LaravelDto\Contracts\HydratorContract;
+use Jovialcore\LaravelDto\Hydrate;
 
 final class  PackageServiceProvider extends ServiceProvider
 {
+
+    public array $bindings  = [
+        HydratorContract::class => Hydrate::class
+    ];
+
+    
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
@@ -20,6 +27,4 @@ final class  PackageServiceProvider extends ServiceProvider
             );
         }
     }
-
-
 }
